e58b9670866d6e25f4536e097d2c230d8dfe531c
Bill Zhang <49241027+bflly1234@users.noreply.github.com>
2022-11-26 21:57:42 -0500

    Update A2FunctionsSummary.py

--- A2FunctionsSummary.py
+++ A2FunctionsSummary.py
@@ -19,7 +19,14 @@ print('Idx-max: ', salesDF[['buy_amount', 'year_old']].idxmax())
 # 2.3.3 Frequency Function
 print('Prov Name: ', salesDF['prov'].unique())
 print('How Many prov: ', salesDF['prov'].nunique())
-# Note: Queries the location of the first occurrence of each variable, ***
+# drop duplicate record, keep='first' means keep first duplicate record ='last' keeps last duplicate keep=false remove both duplicate record
+# import pandas as pd
+# df = pd.DataFrame({'Country ID':[1,1,2,12,34,23,45,34,23,12,2,3,4,1],
+#                     'Age':[12,12,15,18, 19, 25, 21, 25, 25, 18, 25,12,32,18],
+#                    'Group ID':['a','z','c','a','b','s','d','a','b','s','a','d','a','f']})
+# print(df)
+# df = df.drop_duplicates(['Age','Group ID'],keep=False).reset_index(drop=True)
+# print(df)
 print('How Many prov: ', salesDF['prov'].drop_duplicates())
 # 2.3.4 Substitution Function (might need to know more)
 # 2.3.5 Sort Function
